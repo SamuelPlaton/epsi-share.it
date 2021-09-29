16924f4493351aa8b13262dbaf342d0de29311a0
Louis Bernardeau <lbernardeau@la-joliverie.com>
2021-09-29 09:37:35 +0200

    ajout lien rgpd

--- packages/app-shareit/src/pages/CGU.tsx
+++ packages/app-shareit/src/pages/CGU.tsx
@@ -5,11 +5,15 @@ function CGU() {
 
     // @ts-ignore
     return (
-        <form className="form bg-gray-200">
-            <div className="form-group flex-auto justify-items-stretch ">
-                <label className="label text-center text-purple-500 w-full relative">L'application Share.it respecte les conditions générales d'utilisation et préservé la sécurité ainsi que l'intégrité des données utilisateurs qui transitent via nos services conformément à la loi RGPD du 25 mai 2018.</label>
+        <div className='flex flex-col justify-center items-center w-full h-screen py-4 bg-gray-100'>
+            <div className='h-1/3 my-8 flex items-center'>
+                <img src='logo.png' alt='logo' className="relative w-64" />
             </div>
-        </form>
+            <div className="flex flex-col flex-grow ">
+                <label className="label text-center w-full relative">L'application Share'it respecte les conditions générales d'utilisation et préserve la sécurité ainsi que l'intégrité des données utilisateurs qui transitent via nos services conformément à la loi
+                 <a href="https://www.cnil.fr/fr/comprendre-le-rgpd" className="text-blue-500 no-underline hover:underline"> RGPD</a> du 25 mai 2018.</label>
+            </div>
+        </div>
     );
 
 }
