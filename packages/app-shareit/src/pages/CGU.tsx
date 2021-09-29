13d3e488834585506f25730d54e4580c579509ec
Louis Bernardeau <lbernardeau@la-joliverie.com>
2021-09-29 10:04:30 +0200

    ajout navigation layout pour cgu

--- packages/app-shareit/src/pages/CGU.tsx
+++ packages/app-shareit/src/pages/CGU.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import NavigationLayout from "../components/NavigationLayout";
 
 
 function CGU() {
 
     // @ts-ignore
     return (
+        <NavigationLayout title="Conditions générales d'utilisation">
         <div className='flex flex-col justify-center items-center w-full h-screen py-4 bg-gray-100'>
             <div className='h-1/3 my-8 flex items-center'>
                 <img src='logo.png' alt='logo' className="relative w-64" />
@@ -14,6 +16,7 @@ function CGU() {
                  <a href="https://www.cnil.fr/fr/comprendre-le-rgpd" className="text-blue-500 no-underline hover:underline"> RGPD</a> du 25 mai 2018.</label>
             </div>
         </div>
+        </NavigationLayout>
     );
 
 }
